74d99f1842c0c89e5292a1597e0930fb91ea7248
Georgi Iliev <g.iliev@ultraplay.co>
2017-11-02 12:19:02 +0200

    Implemented ServiceLayer and ServiceLayer.Interfaces projects Added some paths to the gitignore file

---
+++ FourthExampleProject/ServiceLayer.Interfaces/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceLayer.Interfaces
+{
+    using Models;
+    using System.Collections.Generic;
+
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetAllCustomers();
+    }
+}
---
+++ FourthExampleProject/ServiceLayer/CustomerService.cs
@@ -0,0 +1,26 @@
+﻿namespace ServiceLayer
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Models;
+    using Interfaces;
+    using DataLayer.Interfaces;
+
+    public class CustomerService : ICustomerService
+    {
+        private IData data;
+
+        public CustomerService(IData data)
+        {
+            this.data = data;
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            IEnumerable<Customer> allCustomers = this.data.Customers.All().ToList();
+
+            return allCustomers;
+        }
+    }
+}
