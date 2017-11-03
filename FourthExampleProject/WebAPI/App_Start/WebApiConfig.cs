a357d4b69b808780d82188437c54826ef26b130e
Georgi Iliev <g.iliev@ultraplay.co>
2017-11-03 20:59:32 +0200

    Enabled CORS on the WebApi

--- FourthExampleProject/WebAPI/App_Start/WebApiConfig.cs
+++ FourthExampleProject/WebAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
 ﻿namespace WebAPI
 {
+    using System;
     using System.Web.Http;
+    using System.Web.Http.Cors;
     using System.Net.Http.Headers;
 
     using Microsoft.Owin.Security.OAuth;
@@ -10,6 +12,9 @@
     {
         public static void Register(HttpConfiguration config)
         {
+            EnableCorsAttribute corsSettings = GetCorsSettings();
+            config.EnableCors(corsSettings);
+
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
@@ -24,6 +29,22 @@
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+
+        /// <summary>
+        /// Returns the CORS settings
+        /// </summary>
+        /// <returns>EnableCorsAttribute</returns>
+        [Obsolete("Do not allow every origin, header and method!")]
+        private static EnableCorsAttribute GetCorsSettings()
+        {
+            string allowedOrigins = "*";
+            string allowedHeaders = "*";
+            string allowedMethods = "*";
+
+            EnableCorsAttribute corsSettings = new EnableCorsAttribute(allowedOrigins, allowedHeaders, allowedMethods);
+
+            return corsSettings;
         }
     }
 }
